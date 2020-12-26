ff6c96c3d5b6062b78fcf0ea047765c3f5ad5012
Andrej Ilievski <mrandrej.ilievski@gmail.com>
2020-12-26 18:46:39 +0100

    Quest 14

---
+++ migrations/Version20201226172001.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201226172001 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE actor (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE actor_program (actor_id INT NOT NULL, program_id INT NOT NULL, INDEX IDX_B01827EE10DAF24A (actor_id), INDEX IDX_B01827EE3EB8070A (program_id), PRIMARY KEY(actor_id, program_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE actor_program ADD CONSTRAINT FK_B01827EE10DAF24A FOREIGN KEY (actor_id) REFERENCES actor (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE actor_program ADD CONSTRAINT FK_B01827EE3EB8070A FOREIGN KEY (program_id) REFERENCES program (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE category DROP programs');
+        $this->addSql('ALTER TABLE episode CHANGE season_id season_id INT DEFAULT NULL, CHANGE number number INT DEFAULT NULL, CHANGE synopsis synopsis LONGTEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE season CHANGE program_id program_id INT DEFAULT NULL, CHANGE number number INT DEFAULT NULL, CHANGE year year INT DEFAULT NULL, CHANGE description description LONGTEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE actor_program DROP FOREIGN KEY FK_B01827EE10DAF24A');
+        $this->addSql('DROP TABLE actor');
+        $this->addSql('DROP TABLE actor_program');
+        $this->addSql('ALTER TABLE category ADD programs VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE episode CHANGE season_id season_id INT NOT NULL, CHANGE number number INT NOT NULL, CHANGE synopsis synopsis LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE season CHANGE program_id program_id INT NOT NULL, CHANGE number number INT NOT NULL, CHANGE year year INT NOT NULL, CHANGE description description LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
---
+++ src/Controller/ActorController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ActorController extends AbstractController
+{
+    /**
+     * @Route("/actor", name="actor")
+     */
+    public function index(): Response
+    {
+        $actors = $this->getDoctrine()
+            ->getRepository(Actor::class)
+            ->findAll();
+
+        return $this->render('actor/index.html.twig', [
+            'controller_name' => 'ActorController',
+            'actors' => $actors
+        ]);
+    }
+
+    /**
+     * @Route("/show/{actorId<^[0-9]+$>}", name="show")
+     * @ParamConverter("actors", class="App\Entity\Actor", options={"mapping": {"actorId": "id"}})
+     */
+    public function show(Actor $actors): Response
+    {
+        $programs = $actors->getPrograms();
+
+        if (!$actors) {
+            throw $this->createNotFoundException(
+                'No program with id : '.($actors).' found in program\'s table.'
+            );
+        }
+
+        return $this->render('actor/show.html.twig', [
+            'programs' => $programs,
+            'actors' => $actors,
+        ]);
+    }
+}
